e60f5584955903257181f9703d3d3bd059f325ed
Matthew McVickar <matthew@matthewmcvickar.com>
2014-05-18 15:07:31 -0700

    Printing default task list to the generated month box.

--- js/main.js
+++ js/main.js
@@ -1,32 +1,84 @@
 $(function() {
 
-  // Form elements.
-  var yearSelect  = $('.year-select'),
-      monthSelect = $('.month-select'),
-      itemsText   = $('.items-text');
+  var yearField      = $('#year'),
+      monthField     = $('#month'),
+      itemsField     = $('#items'),
+      taskpaperMonth = $('#taskpaper-month');
+
+  // Fetch the default item list.
+  function getDefaultItemList() {
+    var defaultItemListData = null;
+
+    $.get('default-item-list.txt', function(data) {
+      defaultItemListData = data;
+    });
+
+    return defaultItemListData;
+  }
+
+  var defaultItemList = getDefaultItemList();
+
+  // Focus on the items textbox on load.
+  itemsField.focus();
 
   // If we have cookies, set the month <select>, year <select>, and
   // item list <textarea> to cookie values.
+
   // TODO
 
-  // Otherwise, populate the items <textarea> with the contents of
-  // the default task list text file.
-  $.get('default-task-list.txt', function(data) {
-    itemsText.html(data);
-  });
+  // Otherwise:
+  // 1. Populate the items <textarea> with the contents of the default item
+  //    list text file.
+  itemsField.html(getDefaultItemList);
 
-  // And populate year <select> with current and next year.
+  // 2. Populate year <select> with current and next year.
   var currentYear = new Date().getFullYear(),
       nextYear    = currentYear + 1,
       years       = [currentYear, nextYear];
 
   $.each(years, function(key, value) {
-    yearSelect.append(
+    yearField.append(
       $('<option></option>').attr('value', value).text(value)
     );
   });
 
-  // Update the generated month live with the translated contents of
-  // the text box.
+  // Build a TaskPaper month with a given year and month.
+  function generateMonth(year, month, items) {
+    var year           = typeof year === 'undefined' ? new Date().getFullYear() : year,
+        month          = typeof month === 'undefined' ? new Date().getMonth() : month,
+        items          = typeof items === 'undefined' ? items : items,
+        numberOfDays   = new Date(year, month, 0).getDate(),
+        generatedMonth = '';
+
+    // Build the array of items from the contents of the textarea.
+    generatedMonth += items;
+
+    // Loop through each of the days and include the items therein.
+    for (var day = 1; day <= numberOfDays; day++) {
+
+      // Add leading zeroes to day numbers under 10.
+      day = day < 10 ? '0' + day : day;
+
+      generatedMonth += day !== '01' ? '\n' : '';
+
+      // Start the day.
+      generatedMonth += day;
+
+      // If this day contains items, print them.
+      // TODO.
+    }
+
+    return generatedMonth;
+  }
+
+  // Update the generated month live with the rebuilt contents of
+  // the item list text box.
+  itemsField.on('keyup', function() {
+    // TODO.
+  });
+
+
+  // Debug output.
+  taskpaperMonth.html(generateMonth(null, null, getDefaultItemList()));
 
 });
